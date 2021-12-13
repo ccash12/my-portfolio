9e262237cabe8148dab2c1eedfff0dd12224e189
Courtney Cash <cashcourtney97@gmail.com>
2021-12-13 13:44:11 -0500

    links work

--- courtney-cash-portfolio/src/components/NavBar.js
+++ courtney-cash-portfolio/src/components/NavBar.js
@@ -1,6 +1,18 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
 function NavBar() {
     return(
         <div>
+            <Link to="/">
+                <h1>Courtney Cash</h1>
+            </Link>
+            <Link to="/projects">
+                <h1>Projects</h1>
+            </Link>
+            <Link to="/contact">
+                <h1>Contact Me</h1>
+            </Link>
 
         </div>
     )
