35973d04e057a2444aad3f7f52af28f79de3de0f
Courtney Cash <cashcourtney97@gmail.com>
2021-12-14 12:01:57 -0500

    fixed routes and switch

--- courtney-cash-portfolio/src/App.js
+++ courtney-cash-portfolio/src/App.js
@@ -1,16 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route} from 'react-router-dom';
 
 //component imports
 
 import NavBar from './components/NavBar'
 import Home from './components/Home'
+import Projects from './components/Projects'
 
 function App() {
   return (
     <div className="App">
-      <NavBar /> 
-      <Home />
+        <NavBar /> 
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- courtney-cash-portfolio/src/components/NavBar.js
+++ courtney-cash-portfolio/src/components/NavBar.js
@@ -4,19 +4,22 @@ import { Link } from "react-router-dom"
 function NavBar() {
     return(
         <div className="NavBarContainer">
-            <Link to="/">
-                <h3>Courtney Cash</h3>
-            </Link>
-            <Link to="/projects">
-                <h3>Projects</h3>
-            </Link>
-            <Link to="featured">
-                <h3>Featured</h3>
-            </Link>
-            <Link to="/contact">
-                <h3>Contact Me</h3>
-            </Link>
-
+            <div className="NavBarTitle">
+                <Link to="/">
+                    <h3>Courtney Cash</h3>
+                </Link>
+            </div>
+            <div classname="NavBarLinks">
+                <Link to="/projects">
+                    <h3>Projects</h3>
+                </Link>
+                <Link to="featured">
+                    <h3>Featured</h3>
+                </Link>
+                <Link to="/contact">
+                    <h3>Contact Me</h3>
+                </Link>
+            </div>
         </div>
     )
 }
