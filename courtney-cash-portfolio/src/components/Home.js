053c5a3d85cfcad09c8d6db18daa5e625e3cc7cc
Courtney Cash <cashcourtney97@gmail.com>
2021-12-15 12:20:08 -0500

    css added

--- courtney-cash-portfolio/src/components/Home.js
+++ courtney-cash-portfolio/src/components/Home.js
@@ -1,9 +1,6 @@
-
-
 function Home() {
     return(
-        <div>
-            <p>Hello! My name is Courtney and this is my portfolio!</p>
+        <div className="HomeContainer">
         </div>
     )
 }
