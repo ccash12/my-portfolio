208873380bd00da21a5a438feca260979b647e32
Courtney Cash <cashcourtney97@gmail.com>
2021-12-15 13:09:48 -0500

    about me done

--- courtney-cash-portfolio/src/components/AboutMe.js
+++ courtney-cash-portfolio/src/components/AboutMe.js
@@ -1,8 +1,18 @@
 function AboutMe() {
     return(
         <div className="AboutMeContainer">
-            <p>Hi! My name is Courtney and I am a full stack software engineer. I am proficient in JavaScript, React JS, Ruby on Rails, Sinatra, HTML and CSS</p>
+            <p>Hi! My name is Courtney and I am a full stack software engineer. I am proficient in JavaScript, React JS, Ruby on Rails, Sinatra, HTML and CSS.
 
+            I received my undergraduate bachelor's degree in Psychology and Sociology. When it was time to graduate I applied to a technology company  to work in support. 
+
+            After working there for 2 years I realized that I wanted to be the one building the applications that I was supporting. I then left my job to attend Flatiron School full time to become a software engineer.
+
+            My time at Flatiron school was the most challenging fifteen weeks of my life. It forced me to think im a completely different way, that I never had before.
+
+            I am excited to continue my journey into the world of coding and cannot wait for what is next!
+            </p>
+            <a href="https://www.linkedin.com/in/courtney-cash-616a8617b/">LinkedIn</a>
+            <a href="https://github.com/ccash12">GitHub</a>
         </div>
     )
 }
