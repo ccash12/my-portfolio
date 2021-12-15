e257f0bddcc6c8660ef8e3d623e134046e3be0eb
Courtney Cash <cashcourtney97@gmail.com>
2021-12-15 11:32:54 -0500

    email service working

--- courtney-cash-portfolio/src/components/ContactMe.js
+++ courtney-cash-portfolio/src/components/ContactMe.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
-import { useState } from 'react'
 
-function ContactMe() {
-    const [name,setName] = useState("")
-    const [email,setEmail] = useState("")
-    const [subject,setSubject] = useState("")
-    const [message,setMessage] = useState("")    
-    function sendEmail(e) {
+
+function ContactMe() {  
+    
+    const sendEmail = (e) => {
         e.preventDefault();
-
-        emailjs.sendForm('gmail', 'template_rkkxzrw', e.target, 'user_vlPzjlDuOayKajM3ZxmBk')
+    
+        emailjs.sendForm('service_guy505c', 'template_rkkxzrw', e.target, 'user_vlPzjlDuOayKajM3ZxmBk')
             .then((result) => {
-            console.log(result.text);
+                console.log(result.text);
             }, (error) => {
-            console.log(error.text);
+                console.log(error.text);
             });
-        e.target.reset()
-    }
+            e.target.reset()
+        };
 
 
     return(
@@ -42,7 +39,7 @@ function ContactMe() {
                 <textarea
                     type="text"
                     name="message"
-                    placeholder="Your Message" >
+                    placeholder="Your Message">
                 </textarea>
                 <input
                     type='submit'
