e6b83f80cfc81e94a8196a03e12356504c61cb4f
Courtney Cash <cashcourtney97@gmail.com>
2021-12-15 13:32:08 -0500

    working on images for projects

--- courtney-cash-portfolio/src/components/Projects.js
+++ courtney-cash-portfolio/src/components/Projects.js
@@ -1,7 +1,17 @@
 function Projects() {
     return(
-        <div> 
+        <div className='ProjectsContainer'> 
+            <div className="CooksImage">
+                <img src="https://lh3.googleusercontent.com/RVjgaqEyZesXBTRfR1Xdukjr8G8FAPXgK0PEhGZHB5Nw9vRvNjIkdzpbo1QFDTWPjrSfXGnXAM4KF7RaTbb3OIUYa2Q_D-Q0ieoN-nxsoVEUKt2hDQ7YvvZhVfI8Gojzr7UIzhv9lZSXxfuAcBrtzLA0i-IiG-kHuMX8UsrU7oYb2swOXvJNpVZUgIrygOZsBUTgPtRSgAz8qZqm3R6QI1iia-SQN3cAUvc8VqFr99RQZZ_zHaGoMepWwUOx--CKH6Z1eeg3IhJ4ZIWfdaB8l0UWzB3pQm_g5nj65whQ1q7Q_DhKCRt8E_G4TnkhcZHF58ijDA5hp2Cc7sUQDHa5h4-IQNXIKY3SnM8Y8hRABFUO0Aef7sSEdHro5pdGPOsoT_DarfXk05OEr1WP-FPdnPVAA9CCJRRRuqq4ft7nsbYy-GB1HdPP27Y21GAtzCaU8586pkKEe1IEOqlxP54kziAdKqGIPqS7Joa5QIQ9C7l7FkpEF7TT7vsir4jrSII9QiT0Y1qsmcgSOTaASar2NT_GDxY3PrjjQBlkxgmz5mXjBHobQPR_eneBQHSpqi72IDs63GVlROrtWs5u-PCHGFOszw2O4m5J0Jaf6ge-4lDqm9e_5NQs7uuwlwWGazZsvQmHyPIJ5WK0MRXxt5rrQeE_K82JIQufTe1Ws7ZdzjkWUO251iPs7yImnlppKHeQlZpqjBzfT3MgmWd6h3zNAzik=w2876-h1372-no?authuser=0" alt="cooks"/>
+            </div>
             <a href="https://a-couple-of-cooks.herokuapp.com/">A Couple of Cooks</a>
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="FoodshipifyImage">
+                <img src="https://lh3.googleusercontent.com/LLwuJKLIzrptonL1aXFhiUI-cg00ZtfkHNAE-cIlcTFu4K2rdHU8mwjzvHh8Mv3Zcz31Ak_Z58eQtk-9zAleYYCipdaXkVRiWpi1IF6LAwZSndYKe3f4xtBkdqoZcOF8QBTknMUBnSDpK2gNP5r8MGHCi4VVY15O1Xwp1AF6t8cVI8TfkwaAhyKU_yAUYXOMhaWPxMHkalsqDdB1ePtEjeGuxgcdP0iGffmUMdyIpX_zPFGVfzhUB6VdQzgp3VCxpXZU91rl0_DN-XOS9GuW7BCXjRo0yCM2uqzFjA6ha8AbvYCoOg-ewIKhyAyRn9fSglr3_akvbMD3ksb-5fN-hOZF_4xFNIW8ge3CQSJ9PHfRYj3euIxBL2x1xr-nktAH-T6Xtl43ZilOcB7Kg-SxHBT-yq_xZE5WKsFVR-r1HFoEs4avmjvIjPGPtgUg_2wYqfmi4sK2FtG-3KpuOF8Dwg_6b8nFHgeLWSg4tVfYbumGOrzuPKPCHwDDg-Ad1LPxQPuFZJDd6DEKGZDyo30sJx6-xA8Gh_SDLGIVmVYYrnh8CF1uw128cVYZ5807tRgPRpItPgViLvBk2xaPB5PDQw84Yw7ylE95IxOv97YAB-JnJbjxdgXPz2uNk-h-no1KCinQ_voJBE3lttYOsELwAi7jYknpyrlVXtmPnGoM85G4ZkzXh0hEMhTIP9aQZEoGGj4III1Cl346xFLVtE_JMGZO=w2880-h1372-no?authuser=0" alt="foodshipify"/>
+            </div>
             <a href="https://foodshipify.herokuapp.com/">FoodShipify</a>
             <h1>Ambient Art Gallery </h1>
         </div>
